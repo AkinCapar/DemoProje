525628cb2f62b2da4ba7e3743279c3eea93410e9
Akin Capar <akincapar2@gmail.com>
2021-03-13 20:49:12 +0300

    Player and animations generated.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float followSpeed = 0.125f;
+    [SerializeField] Vector3 offSet;
+
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offSet;
+    }
+
+}
