1b45d7b2ed1994c8bf458e9b1652cb149c8d820d
Akin Capar <akincapar2@gmail.com>
2021-03-14 00:19:50 +0300

    Swerve mechanic basics done.

---
+++ Assets/Scripts/SwerveMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwerveMovement : MonoBehaviour
+{
+    private SwervingInputSystem swervingInputSystem;
+    [SerializeField] private float swerveSpeed = 0.5f;
+
+    private void Awake()
+    {
+        swervingInputSystem = GetComponent<SwervingInputSystem>();
+    }
+
+
+    private void Update()
+    {
+        float swerveAmount = Time.deltaTime * swerveSpeed * swervingInputSystem.MoveFactorX;
+        transform.Translate(swerveAmount, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/SwervingInputSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwervingInputSystem : MonoBehaviour
+{
+    private float lastFrameFingerPositionX;
+    private float moveFactorX ;
+    public float MoveFactorX => moveFactorX;
+    public float runSpeed = 1f;
+    Animator myAnimator;
+
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastFrameFingerPositionX = Input.mousePosition.x;
+        }
+
+        else if(Input.GetMouseButton(0))
+        {
+            myAnimator.SetBool("isRunning", true);
+            moveFactorX = Input.mousePosition.x - lastFrameFingerPositionX;
+            transform.position += transform.forward * Time.deltaTime * runSpeed;
+            lastFrameFingerPositionX = Input.mousePosition.x;
+
+        }
+
+        else if(Input.GetMouseButtonUp(0))
+        {
+            moveFactorX = 0f; 
+        }
+
+        else
+        {
+            myAnimator.SetBool("isRunning", false);
+        }
+    }
+}
